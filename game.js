fa0572651f443c0dd01d0c306d93f2438792ebc4
Cameron Koster <kostercam37@gmail.com>
2018-11-02 16:55:53 -0600

    buttons and functions

---
+++ game.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+let health = 100;
+
+function slap() {
+  health--
+  update()
+}
+
+function punch() {
+  health--
+  update()
+}
+
+function kick() {
+  health--
+  update()
+}
+
+
+
+function update() {
+
+  document.getElementById("health").innerText = health
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+update()
