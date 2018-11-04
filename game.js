b55b2afa5c967b28cbc304fe23885db0d1ce5c85
Cameron Koster <kostercam37@gmail.com>
2018-11-03 19:09:51 -0600

    lost

--- game.js
+++ game.js
@@ -1,35 +1,55 @@
+let items = {
+  kraken: { name: "Release Kraken", modifier: 2, description: "Releases a nasty Kraken" },
+  insult: { name: "Add Insult", modifier: 2, description: "Adds insult to injury" },
+  kickDown: { name: "Kick While Down", modifier: 2, description: "Kicks 'em while they're down" }
+}
+
+let josh = {
+  name: "Josh",
+  health: 100,
+  hits: 0,
+  items: [items.kraken, items.insult, items.kickDown]
+}
 
 
-let health = 100;
-let enemy = "Josh";
-let hits = 0;
+
+function addSpecials() {
+  let specialsTotal = 0; //need the singular/plural card broken down for me. is "i" always "i", or is there any scenario I HAVE to use a var name?
+  for (i = 0; i < items.length; i++) {
+    let item = items[i]//I'm supposed to access the array using [i] here. How?
+    specialsTotal += 2
+    return specialsTotal
+
+  }
+
+}
+
+function giveKraken() {
+  items.push(items.kraken) //need the order of these. is the first item, in this example "items" the array we're pushing onto, 
+  //or the object we're pulling from? Also how is this differentiating between the property "items" or the full object "items?"
+}
 
 
 
 function slap() {
-  health--
+  josh.health -= 1
   update()
 }
 
 function punch() {
-  health -= 5
+  josh.health -= 5
   update()
 }
 
 function kick() {
-  health -= 10
+  josh.health -= 10
   update()
 }
 
 
 
 function update() {
-
-
-
-  document.getElementById("health").innerText = health
-  document.getElementById("enemy").innerText = enemy
-  document.getElementById("hits").innerText = hits
+  document.getElementById("health").innerText = josh.health
 }
 
 
