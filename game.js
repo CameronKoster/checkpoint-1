84ceec300c7096d8af5c35a3a11cff9d0a3b239c
Cameron Koster <kostercam37@gmail.com>
2018-11-04 17:49:18 -0700

    passing

--- game.js
+++ game.js
@@ -1,55 +1,90 @@
 let items = {
-  kraken: { name: "Release Kraken", modifier: 2, description: "Releases a nasty Kraken" },
+  kraken: { name: "Release Kraken", modifier: 7, description: "Releases a nasty Kraken" },
   insult: { name: "Add Insult", modifier: 2, description: "Adds insult to injury" },
-  kickDown: { name: "Kick While Down", modifier: 2, description: "Kicks 'em while they're down" }
+  kickDown: { name: "Kick While Down", modifier: 3, description: "Kicks 'em while they're down" }
 }
 
 let josh = {
   name: "Josh",
   health: 100,
   hits: 0,
-  items: [items.kraken, items.insult, items.kickDown]
+  items: []
 }
 
 
 
 function addSpecials() {
   let specialsTotal = 0; //need the singular/plural card broken down for me. is "i" always "i", or is there any scenario I HAVE to use a var name?
-  for (i = 0; i < items.length; i++) {
+  for (let i = 0; i < josh.items.length; i++) {
     let item = items[i]//I'm supposed to access the array using [i] here. How?
-    specialsTotal += 2
-    return specialsTotal
+    specialsTotal += josh.items[i]
+
 
   }
-
+  return specialsTotal
 }
 
 function giveKraken() {
-  items.push(items.kraken) //need the order of these. is the first item, in this example "items" the array we're pushing onto, 
+  josh.items.push(items.kraken.modifier)
+  //need the order of these. is the first item, in this example "items" the array we're pushing onto,
   //or the object we're pulling from? Also how is this differentiating between the property "items" or the full object "items?"
 }
 
+function giveInsult() {
+  josh.items.push(items.insult.modifier)
+}
+
+function giveKickDown() {
+  josh.items.push(items.kickDown.modifier)
+}
 
 
-function slap() {
-  josh.health -= 1
+function front() {
+  if (josh.items.length > 0) {
+    josh.health -= 1 * addSpecials()
+  }
+  else {
+    josh.health -= 1
+  }
+  josh.hits++
+  update()
+
+}
+
+function sock() {
+  if (josh.items.length > 0) {
+    josh.health -= 5 * addSpecials()
+  }
+  else {
+    josh.health -= 5
+  }
+  josh.hits++
   update()
 }
 
-function punch() {
-  josh.health -= 5
+function loogie() {
+  if (josh.items.length > 0) {
+    josh.health -= 10 * addSpecials()
+  }
+  else {
+    josh.health -= 10
+  }
+  josh.hits++
   update()
 }
 
-function kick() {
-  josh.health -= 10
-  update()
+function dead() {
+  if (josh.health <= 0) {
+    josh.health = 0
+    alert("JOSH SUCKS!!!!")
+  }
 }
-
 
 
 function update() {
-  document.getElementById("health").innerText = josh.health
+  dead()
+  document.getElementById("health").innerText = josh.health.toString();
+  document.getElementById("hits").innerText = josh.hits.toString();
 }
 
 
